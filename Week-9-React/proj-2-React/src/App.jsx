8bbe90386e4fa217a2429cbe19cdedb64b79aa1a
Nihal Bhardwaj <bhardwajnihal00721@gmail.com>
2024-10-13 16:46:20 +0530

    understood side effects and useEffects in detail

--- Week-9-React/proj-2-React/src/App.jsx
+++ Week-9-React/proj-2-React/src/App.jsx
@@ -1,4 +1,4 @@
-import { useDebugValue, useEffect, useState } from "react"
+import { useDebugValue, useEffect, useState, useTransition } from "react"
 import { PostComponent } from "./post"
 import { FoodComponents } from "./React-arrayMap"
 
@@ -10,11 +10,14 @@ function App() {
     //each component has their own state variable, that changes , and components get re-rendered separately
     <div >
 
-        < Posts/>
+        {/* < Posts/> */}
+        {/* <Notification /> */}
 
         {/* <ToggleMessage /> */}
-        
-        <Notification />
+          
+        {/* <Count /> */}
+
+        <Navbar />
     </div>
     
   )
@@ -113,22 +116,22 @@ function ToggleMessage(){
     }
 
     // Cleanup the interval on component unmount
-    // useEffect(() => {
-    //   return()=>{
-    //     if (currentInterval !== null) {
-    //       console.log("current interval is not null, now cleanup is needed on component unmount");
+    useEffect(() => {
+      return()=>{
+        if (currentInterval !== null) {
+          console.log("current interval is not null, now cleanup is needed on component unmount");
           
-    //       clearInterval(currentInterval); // Cleanup any active intervals
-    //     }
-    //   };
-    // }, [currentInterval]); // The cleanup function will run when intervalId changes or component unmounts
+          clearInterval(currentInterval); // Cleanup any active intervals
+        }
+      };
+    }, [currentInterval]); // The cleanup function will run when intervalId changes or component unmounts
 
     return (
       <div>
         <div
           className="notificationCount"
           style={{
-            position: "absolute",
+            position: "fixed",
             top: 103,
             left: 117,
             zIndex: 10,
@@ -142,7 +145,7 @@ function ToggleMessage(){
           {currentNotificationCount}
         </div>
         <img
-          style={{ height: 30, width: 38, position: "absolute", top: 105, left: 100 }}
+          style={{ height: 30, width: 38, position: "fixed", top: 105, left: 100 }}
           src="https://flyclipart.com/thumb2/bell-icon-png-and-vector-for-free-download-545512.png"
           alt="Notification bell"
         />
@@ -156,5 +159,84 @@ function ToggleMessage(){
     );
   }
 
+// Dependency Array:
+  function Count(){
+    
+    const [count1, setcount1] = useState(0)
+    const [count2, setcount2] = useState(0)
+
+    useEffect(()=>{
+      setInterval(() => {
+        setcount1(count1 => count1 + 1)
+      }, 1000);
+    },[])
+
+    useEffect(()=>{
+      setInterval(() => {
+        setcount2(count2 => count2 + 1)
+      }, 5000);
+    },[])
+
+// Dependency array takes that component as an argument, which triggers an action on being re-rendered
+    useEffect(()=>{
+      console.log("page reloaded!!!");    
+    },[])                     // empty array depicts that any action is performed only once when the component is mounted or when the page reloads
+
+
+    useEffect(()=>{
+      console.log("count 1 changed");     //gets logged everytime count1 get re-rendered
+    },[count1])
+
+    useEffect(()=>{
+      console.log("count 2 changed");       //gets logged everytime count2 get re-rendered
+    },[count2])
+
+    useEffect(()=>{
+      console.log("one of the count changed this time!!!!");       //gets logged everytime either of count get re-rendered
+    },[count1,count2])
+
+    return <div>
+      <h3>count1 : {count1}</h3>
+      <h3>count2 : {count2}</h3>
+    </div>
+  }
+
+//Navbar component:
+function Navbar(){
+  const[currentTab, setCurrentTab] = useState()
+  const[responseData, SetResponseData] = useState()
+  const[IsLoading, setLoader] = useState()
+
+  function changeTab(id){
+    setCurrentTab(id)
+  }  
+// Thus we can run side effects whenever a particular element is clicked like - API request, some js logic, etc 
+  useEffect(()=>{
+    if(currentTab){
+      console.log("request send to the backend when clicked", currentTab);
+    }
+
+    setLoader(true)
+  fetch(`https://jsonplaceholder.typicode.com/todos/${currentTab}`)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      SetResponseData(data.title) 
+    })
+    .then(() => setLoader(false))
+
+  },[currentTab])
+
+  return <div>
+  <div style={{display:"flex", gap:5}}>
+   <button onClick={() => changeTab(1)} style={{padding:8, color : currentTab==1 ? "red" : "black"}}>Feeds</button>
+    <button onClick={() => changeTab(2)} style={{padding:8 ,color : currentTab==2 ? "red" : "black"}} >Notifications</button>
+    <button onClick={() => changeTab(3)} style={{padding:8,color : currentTab==3 ? "red" : "black"}}>Messages</button>
+    <button onClick={() => changeTab(4)} style={{padding:8,color : currentTab==4 ? "red" : "black"}}>Jobs</button>
+  </div>
+  {currentTab &&<div style={{width:500,backgroundColor:"#ddd", margin:10}}><h2>title :{(IsLoading) ? "loading..." : responseData} </h2></div>}
+  </div>
+}
+  
 
 export default App

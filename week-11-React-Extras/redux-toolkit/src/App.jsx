47348e01a747ead2edf2c4be207ddd5647fe66d1
Nihal Bhardwaj <bhardwajnihal00721@gmail.com>
2024-12-04 13:00:28 +0530

    understood state management using react-redux and redux-toolkit

--- week-11-React-Extras/redux-toolkit/src/App.jsx
+++ week-11-React-Extras/redux-toolkit/src/App.jsx
@@ -1,11 +1,18 @@
 
+import { useSelector, useDispatch} from 'react-redux'
+import { increment, decrement } from './setup/2-slice'
 
 function App() {
   
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch()
 
   return (
     <>
-      <h2>hello</h2>
+      <h2>react redux</h2>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
     </>
   )
 }
